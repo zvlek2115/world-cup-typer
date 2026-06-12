a67490073fc603b71cf9b371fe50949bb556ce51
zylek <zylek2115@gmail.com>
2026-06-13 00:28:01 +0200

    Move railway.json back to root

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,6 +6,13 @@ import app from './app';
 const PORT = process.env.PORT || 5000;
 
 // Server starting point
-app.listen(Number(PORT), '0.0.0.0', () => {
+const server = app.listen(Number(PORT), '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received: closing HTTP server');
+  server.close(() => {
+    console.log('HTTP server closed');
+  });
 });
